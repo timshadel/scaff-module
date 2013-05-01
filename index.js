aa7eb134adc8d1f82547abed2cb35838d14b0609
Tim Shadel <tim@shadelsoftware.com>
2013-04-30 21:43:26 -0600

    Try out some dates!

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+function formatDate(d){
+  function pad(n){return n<10 ? '0'+n : n}
+  return d.getFullYear()+'-'
+      + pad(d.getMonth()+1)+'-'
+      + pad(d.getDate());
+}
+
+var today = new Date();
+
 exports.variables = {
-  description: "Project description: "
-, author: "Enter your name: "
-, email: "Enter your email: "
-, username: "Enter your github username: "
+    description: "Project description: "
+  , author: "Enter your name: "
+  , email: "Enter your email: "
+  , username: "Enter your github username: "
+  , date: formatDate(today)
+  , year: today.getFullYear()
 };
