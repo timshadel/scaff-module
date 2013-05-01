22ff5f75dea9b152b5f7d440be043188f43110c4
Tim Shadel <tim@shadelsoftware.com>
2013-04-30 21:32:10 -0600

    Make the username available

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ exports.variables = {
   description: "Project description: "
 , author: "Enter your name: "
 , email: "Enter your email: "
-, github_user: "Enter your github username: "
+, username: "Enter your github username: "
 };
